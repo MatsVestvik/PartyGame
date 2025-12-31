8069fe1cf624b7c53f388277523117268a0421b8
MatsVestvik <mats.o.vestvik@gmail.com>
2025-12-31 13:57:25 +0100

    added character class and superstar subclass

---
+++ src/main/java/characters/Character.java
@@ -0,0 +1,14 @@
+package characters;
+
+public class Character {
+    public String name;
+    public int trouble;
+    public int cash;
+
+    public Character(String name, int trouble, int cash) {
+        this.name = name;
+        this.trouble = trouble;
+        this.cash = cash;
+    }
+}
+
---
+++ src/main/java/characters/SuperStar.java
@@ -0,0 +1,11 @@
+package characters;
+
+public class SuperStar extends Character {
+    public int Stars;
+
+    public SuperStar(String name, int trouble, int cash, int Stars) {
+        super(name, trouble, cash);
+        this.Stars = Stars;
+    }
+    
+}
