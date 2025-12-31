7d507ae612195775c9ec4d0c05bf20a4d8572276
MatsVestvik <mats.o.vestvik@gmail.com>
2025-12-31 13:43:07 +0100

    fixed scene creating roundscene

--- src/main/java/scenes/RoundScene.java
+++ src/main/java/scenes/RoundScene.java
@@ -1,5 +1,8 @@
 package scenes;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,6 +17,17 @@ public class RoundScene {
         stage.setScene(scene);
     }
     public Scene createRoundScene() {
-        return new Scene(null); // Placeholder
+        stage.setTitle("Round Scene");
+        // Implement the round scene UI here
+        // For demonstration, we'll just create a simple scene
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(20));
+
+        Label roundLabel = new Label("This is the Round Scene!");
+        roundLabel.setStyle("-fx-font-size: 24; -fx-font-weight: bold;");
+
+        root.getChildren().add(roundLabel);
+        Scene scene = new Scene(root, 400, 300);
+        return scene;
     }    
 }
