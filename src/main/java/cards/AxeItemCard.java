5551dedceb117f458978fe51884b8e807c50f212
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-05 21:13:07 +0100

    added itemcard

---
+++ src/main/java/cards/AxeItemCard.java
@@ -0,0 +1,9 @@
+package cards;
+
+public class AxeItemCard extends Base {
+    public AxeItemCard(int scale) {
+          super(0, 2, 0, 0, scale, 1, 1, 
+          "axe","This Item can be used to remove a Creature from the club.");
+    }
+    
+}
--- src/main/java/cards/RandomCard.java
+++ src/main/java/cards/RandomCard.java
@@ -4,11 +4,17 @@ public class RandomCard {
    public RandomCard(int scale) {
    } 
    public static Base getRandomCard(int scale) {
-       int rand = (int)(Math.random() * 2);
+       int rand = (int)(Math.random() * 3);
        if (rand == 0) {
            return new BasicCreatureCard(scale);
-       } else {
+       } else if (rand == 1) {
            return new DelinquantCreatureCard(scale);
+       }
+       else if (rand == 2) {
+           return new AxeItemCard(scale);
+       }
+       else {
+           return new BasicCreatureCard(scale);
        }
    }
 }
